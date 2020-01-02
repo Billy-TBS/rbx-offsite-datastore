2650181cd886023e7990ff05172b82e04f39c1f5
Billy_BD <44958196+BillyBD@users.noreply.github.com>
2020-01-02 17:05:07 -0500

    Update data.lua

--- lua/data.lua
+++ lua/data.lua
@@ -3,7 +3,7 @@ Example Script:
 local module = require(script.Parent.Module)
 
 game.Players.PlayerAdded:Connect(function(plr)
-    local data = module.GetData(tostring(plr.UserId, playercash))
+    local data = module.GetData(tostring(plr.UserId, "playercash"))
     plr.leaderstats.Cash.Value = tonumber(data)
 end
 ]]
