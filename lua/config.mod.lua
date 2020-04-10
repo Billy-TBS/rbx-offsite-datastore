2317d4ac693368d11a066bf0bc7b86bf8c0a3757
Billy_BD <44958196+BillyBD@users.noreply.github.com>
2020-04-10 12:25:32 -0400

    Update config.mod.lua

--- lua/config.mod.lua
+++ lua/config.mod.lua
@@ -1,5 +1,5 @@
 -- Put inside of the Data Script. This is a Module Script.
 return {
-    ["url"] = "", -- Make sure the end of the url has a "/" at the end or the script won't work.
-    ["authorization_key"] = "" -- Just smash your keyboard for this.
+    ["url"] = "", -- Make sure the end of the url has a "/" at the end or the script won't work. Also check to make sure the beginning has http:// or https:// or it won't work.
+    ["authorization_key"] = "" -- Just smash your keyboard for this. 
 }
