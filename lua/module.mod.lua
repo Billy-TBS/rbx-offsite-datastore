7b5a018fa22b7c7f255f4470a3f1c65d9eaf877a
Billy_BD <44958196+BillyBD@users.noreply.github.com>
2020-05-18 20:24:33 -0400

    Update module.mod.lua

--- lua/module.mod.lua
+++ lua/module.mod.lua
@@ -27,7 +27,7 @@ local function Request(TypeOfRequest, RequestInfo)
 end
 
 
-function mod.GetData(userid, typofdata)
+mod.GetData = function(userid, typofdata)
 	assert(typeof(userid) == "string", "Error: UserId must be a string!") -- UserId has to be a string because fs checks folders as a string, you could always do this on the server, but you may as well just get it done here.
 	assert(typeof(typofdata) == "string", "Error: Type of Data must be a string!")
 	local Body = {
@@ -40,7 +40,7 @@ function mod.GetData(userid, typofdata)
 	print(result)
 	return result
 end
-function mod.SetData(userid, typofdata, datae)
+mod.SetData = function(userid, typofdata, datae)
 	assert(typeof(userid) == "string", "An error occured, UserId must be a string!")
 	assert(typeof(typofdata) == "string", "An error occured, Type of Data must be a string!")
 	assert(typeof(datae) == "string", "An error occured, Data must be a string!")
