7ade79c37b46126140e2ffa5d1e37ff361c31af7
William "Billy" Anderman <billy@tableboardstudios.com>
2022-02-04 10:40:19 -0500

    Supports "GetFilesForUser"
    Added support for "GetFilesForUser" function.

--- lua/module.mod.lua
+++ lua/module.mod.lua
@@ -1,5 +1,3 @@
--- Put this inside of the Data Script. This is a Module Script
-
 local mod = {}
 local http = game:GetService('HttpService')
 
@@ -31,27 +29,36 @@ mod.GetData = function(userid, typofdata)
 	assert(typeof(userid) == "string", "Error: UserId must be a string!") -- UserId has to be a string because fs checks folders as a string, you could always do this on the server, but you may as well just get it done here.
 	assert(typeof(typofdata) == "string", "Error: Type of Data must be a string!")
 	local Body = {
-			uid = userid,
-			dataname = typofdata..".txt"
+		uid = userid,
+		dataname = typofdata..".txt"
 	}
 	local success, result = pcall(function()
 		return Request('GetData', Body)
 	end)
 	print(result)
-	return result
 end
 mod.SetData = function(userid, typofdata, datae)
 	assert(typeof(userid) == "string", "An error occured, UserId must be a string!")
 	assert(typeof(typofdata) == "string", "An error occured, Type of Data must be a string!")
 	assert(typeof(datae) == "string", "An error occured, Data must be a string!")
 	local Body = {
-			uid = userid,
-			dataname = typofdata..".txt",
-			data = datae
+		uid = userid,
+		dataname = typofdata..".txt",
+		data = datae
 	}
 	local success, result = pcall(function()
 		return Request('SetData', Body)
 	end)
 	print(result)
+end
+mod.GetFilesForUser = function(userid)
+	assert(typeof(userid) == "string", "Error: UserId must be a string!")
+	local Body = {
+		uid = userid
+	}
+	local s, r = pcall(function()
+		return Request('GetFilesForUser', Body)
+	end)
+	print(r)
 end
 return mod
